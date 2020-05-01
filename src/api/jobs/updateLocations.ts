fdb64858d3998cdd0a746b1a2f2e74df2358466b
Aditya Pratap <adityapratap7344@gmail.com>
2020-05-01 07:38:56 +0530

    removed locationUpdate job from server.ts and calling it in the updateLocations job file itself, as we will use heroku scheduler later to schedule it

--- src/api/jobs/updateLocations.ts
+++ src/api/jobs/updateLocations.ts
@@ -181,3 +181,4 @@ export const retrieveLocationsAndUpdateDB = async (): Promise<string | object> =
     }
 }
 
+retrieveLocationsAndUpdateDB();
--- src/server.ts
+++ src/server.ts
@@ -1,23 +1,11 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application } from 'express';
 import api from './api/api';
 
-//import all jobs
-import { retrieveLocationsAndUpdateDB } from './api/jobs/';
 
 const app: Application = express();
 const port: string | number = process.env.PORT || 3000;
 
-
 app.use('/api', api);
-
-
-// execute jobs
-
-retrieveLocationsAndUpdateDB();
-setInterval(() => {
-    // console.log('interval ', Date.now())
-    retrieveLocationsAndUpdateDB();
-}, 2 * 60 * 60 * 1000);
 
 app.listen(port, (): void => {
     console.log(`Server running on port:${port}`);
